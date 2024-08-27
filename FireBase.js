65815a67e2d84a0a8c8e2ade64cb4aa2ccf89149
Henrickxz <145411314+Henrickxz@users.noreply.github.com>
2024-08-26 22:48:33 -0300

    Add files via upload

---
+++ FireBase.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDNPapjF0d37sZa4eRiBaHhAVPrAZXpb6k",
+  authDomain: "appcripto-acdaa.firebaseapp.com",
+  projectId: "appcripto-acdaa",
+  storageBucket: "appcripto-acdaa.appspot.com",
+  messagingSenderId: "848343270748",
+  appId: "1:848343270748:web:4a5e522867fb6b8e93ed99"
+};
+
+const app = initializeApp(firebaseConfig);
